8b69cb7374e0bd458d9fe56e4983d61d0dc978ad
Ha Dinh <dinhhn.ubc@gmail.com>
2018-03-10 16:42:14 -0800

    Finished joinning data

--- src/clean.R
+++ src/clean.R
@@ -1,15 +1,30 @@
 library(tidyverse)
+library(googledrive)
 
 # read data
 assessment <- read_csv("data/raw-data/assessments.csv")
-courses <- read_csv("data/raw-data/courses.csv")
+# courses <- read_csv("data/raw-data/courses.csv")
 studentAssess <- read_csv("data/raw-data/studentAssessment.csv")
 studentInfo <- read_csv("data/raw-data/studentInfo.csv")
 studentRegistration <- read_csv("data/raw-data/studentRegistration.csv")
 studentVle <- read_csv("data/raw-data/studentVle.csv")
 vle <- read_csv("data/raw-data/vle.csv")
 
-# join data 
-master <- left_join(studentAssess, studentRegistration) %>% 
-  left_join(., studentInfo) %>% 
-  left_join(., studentVle)
+# clean data
+studentInfo_c <- studentInfo %>% 
+  select(-studied_credits) %>% 
+  filter(code_presentation %in% c("2014B", "2014J"),
+         final_result %in% c("Pass", "Withdrawn"))
+
+master_assessment <- left_join(assessment, studentAssess)
+
+master <- left_join(studentInfo_c, studentRegistration) %>% 
+  left_join(., studentVle) %>% 
+  left_join(., vle) %>% 
+  left_join(., master_assessment, by = c("code_module", "code_presentation", "id_student"))
+
+master_filtered <- master %>% 
+  select(-date.x, -id_site, -id_assessment, -is_banked,
+         -week_from, -week_to) %>% 
+  rename(deadline = date.y)
+
